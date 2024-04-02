86ec12c7a87fa6007ff51a53b10125d91e8cc6e0
jgomez-17 <jgomez.eng@gmail.com>
2024-04-02 16:26:48 -0500

    02-04-24.1

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -15,13 +15,15 @@ export default [
       },
     },
   },
-  // {
-  //   name: 'strapi::cors',
-  //   config: {
-  //     origin: ['http://localhost:3000', 'https://prontowash-api.onrender.com/'], // Agrega aquí los orígenes permitidos
-  //   },
-  // },
-  'strapi::cors',
+  'strapi::cors', {
+    enabled: true,
+    config: {
+      origin: ['http://localhost:3000'], // Añade tu URL de desarrollo local aquí
+      headers: ['Content-Type', 'Authorization', 'X-Frame-Options', 'Origin'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
